b148d835e5785f96ff2e38ac745022a783505221
Pablo La Rocca <pablolarocca31@gmail.com>
2022-11-21 21:48:34 -0300

    Variables de Entorno & dotenv & README

---
+++ README.md
@@ -0,0 +1,6 @@
+
+### Enviroment variables
+
+para que funcione esta aplicación requieres de un archivo .env en el cual declares las variables de entorno: 
+MONGODB_URI: Dirección de mongodb local (27017) o por Mongodb Atlas URL 
+PORT: Puerto para iniciar la app
--- src/database.js
+++ src/database.js
@@ -1,10 +1,11 @@
-import {connect} from "mongoose";
+import { connect } from "mongoose";
+import { MONGODB_URI } from './config';
 
 // Esta db se va a crear por defecto si no existe
 // conexión asincrona, función anonima
 ( async () => {
     try {
-    const db = await connect("mongodb://localhost/crud-mongo");
+    const db = await connect(MONGODB_URI);
     //const db = await connect("URL DB ATLAS");
     //const db = await connect("URL DB DOCKER");
     console.log('DB connected to', db.connection.name);
