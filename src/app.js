f7b1f6b3e32c6d4ec8d3726329e482a3b587ad3f
Pablo La Rocca <pablolarocca31@gmail.com>
2022-12-10 02:00:30 -0300

    boostrap

--- src/app.js
+++ src/app.js
@@ -1,21 +1,15 @@
-// const express = require("express");
-// npm start src/index.js
-// npx babel-node src/index.js
 import path from "path";
 import express from "express";
-import indexRoutes from "./routes/index.routes";
+import morgan from "morgan";
 import { create } from "express-handlebars";
-import morgan from 'morgan';
+
+import indexRoutes from "./routes/tasks.routes";
 
 const app = express();
 
 // settings
 app.set("port", process.env.PORT || 3000);
-
-// Definimos la ubicacion de la carpeta views
 app.set("views", path.join(__dirname, "views"));
-
-// Definimos el sistema de plantillas y la ubicación
 app.engine(
   ".hbs",
   create({
@@ -25,14 +19,20 @@ app.engine(
     extname: ".hbs",
   }).engine
 );
-// Avisamos del uso de handlebars
 app.set("view engine", ".hbs");
 
 // middlewares
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 
-// Utilizamos las rutas creadas en routes/index.routes
+// routes
 app.use(indexRoutes);
 
-export default app; 
+// public route
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  res.status(404).render("404");
+});
+
+export default app;
