5548345be828812928a32a268e9d18d7971be3d0
Pablo La Rocca <pablolarocca31@gmail.com>
2022-12-10 11:01:26 -0300

    detalles

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,57 +1,48 @@
-import Task from "../models/Task";
+import Task from "../model/Task";
 
 export const renderTasks = async (req, res) => {
-  const tasks = await Task.find().lean();
-  res.render("index", { tasks: tasks });
+  try {
+    const tasks = await Task.find().lean();
+    res.render("index", {
+      tasks,
+    });
+  } catch (error) {
+    console.log({ error });
+    return res.render("error", { errorMessage: error.message });
+  }
 };
 
-export const createTask = async (req, res) => {
+export const createTask = async (req, res, next) => {
   try {
-    const task = Task(req.body);
-
-    // Añadir a mongodb
+    const task = new Task(req.body);
     await task.save();
-
     res.redirect("/");
   } catch (error) {
-    console.log(error);
+    return res.render("error", { errorMessage: error.message });
   }
 };
 
-export const renderTaskEdit = async (req, res) => {
-  try {
-    const task = await Task.findById(req.params.id).lean();
-    res.render("edit", { task: task });
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const editTask = async (req, res) => {
-  const { id } = req.params;
-
-  await Task.findByIdAndUpdate(id, req.body);
-
-  res.redirect("/");
-};
-
-export const deleteTask = async (req, res) => {
-  const { id } = req.params;
-
-  await Task.findByIdAndRemove(id);
-
-  res.redirect("/");
-};
-
-export const taskToggleDone = async (req, res) => {
-  const { id } = req.params;
-
-  // await Task.findByIdAndUpdate(id, { done: !req.body.done } );
+export const taskToggleDone = async (req, res, next) => {
+  let { id } = req.params;
   const task = await Task.findById(id);
-
   task.done = !task.done;
-
   await task.save();
+  res.redirect("/");
+};
 
+export const renderTaskEdit = async (req, res, next) => {
+  const task = await Task.findById(req.params.id).lean();
+  res.render("edit", { task });
+};
+
+export const editTask = async (req, res, next) => {
+  const { id } = req.params;
+  await Task.updateOne({ _id: id }, req.body);
+  res.redirect("/");
+};
+
+export const deleteTask = async (req, res, next) => {
+  let { id } = req.params;
+  await Task.remove({ _id: id });
   res.redirect("/");
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import app from "./app";
-import './database';
-import { PORT } from './config';
+//import './database';
+//import { PORT } from './config';
+import "./utils/mongoose";
 
-app.listen(PORT);
-console.log("Server on port", PORT);
+app.listen(app.get("port"));
+console.log(`server on port ${app.get("port")}`);
